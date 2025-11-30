dabac216193ead524311b9ab9e31eb770a2b3e6c
hanya116 <hanyaali.116@gmail.com>
2025-11-30 21:46:56 +0500

    SimplefunctionArray.java

---
+++ SimplefunctionArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class SimplefunctionArray{
+    public static void main(String[] args){
+        int array[]={1,2,3,4,5};
+        int res = 0;
+        for(int i:array){
+            res += i;
+        }
+        System.out.println("Array ="+ Arrays.toString(array));
+        System.out.println("Sum of Array ="+ " " +res);
+        
+        }
+    }
