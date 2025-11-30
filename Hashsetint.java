0d36e209b04c9ff64f355821190922f334851069
hanya116 <hanyaali.116@gmail.com>
2025-11-30 23:43:09 +0500

    Hashsetint.java

---
+++ Hashsetint.java
@@ -0,0 +1,18 @@
+//ismein int ke bajaye Integer use hoga
+import java.util.HashSet;
+public class Hashsetint{
+    public static void main(String[] args){
+     HashSet<Integer> prime=new HashSet<Integer>();
+     prime.add(2);
+     prime.add(3);
+     prime.add(5);
+     prime.add(7);
+     for (int i = 1; i <= 10; i++) {
+      if (prime.contains(i)) {
+        System.out.println(i + " was found in the set.");
+      } else {
+        System.out.println(i + " was not found in the set.");
+      }
+    }
+  }
+}
